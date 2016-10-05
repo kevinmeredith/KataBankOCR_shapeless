ae9e8ad00bbe1e50aa274ff763becba813884126
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-10-05 10:13:44 -0400

    first

---
+++ build.sbt
@@ -0,0 +1,5 @@
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "com.chuusai" %% "shapeless" % "2.3.2"
+)
---
+++ src/main/scala/net/Main.scala
@@ -0,0 +1,21 @@
+package net
+
+import shapeless._
+import nat._
+import ops.hlist.Length, ops.nat.{ Mod, Prod, Sum }
+
+trait HasChecksum[L <: HList, S <: Nat]
+object HasChecksum {
+  implicit val hasCheckSum
+}
+
+object Main {
+
+  //def nineNatsLtEq9[N <: Nat, L <: HList](implicit ev: Sized[L, _9], ev2: LTEq[N, _9]) = ???
+
+  def isValid[L <: HList](implicit ev: Sized[L, _9], ev2: Length[L]) = ???
+
+  // checksum calculation:
+  //(d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
+
+}
