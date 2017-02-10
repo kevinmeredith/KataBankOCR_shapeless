b586f744017e6198eed4b7caa0144097e325fde7
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-02-09 22:39:38 -0500

    More work

--- src/main/scala/net/IsValid.scala
+++ src/main/scala/net/IsValid.scala
@@ -1,23 +1,44 @@
 package net
 
-import shapeless._, ops.hlist.IsHCons
+import shapeless.{::, HNil, _}
+import ops.hlist.IsHCons
 import nat._
-import ops.nat.{ Mod, Prod, Sum }
+import ops.nat.{Mod, Prod, Sum}
 
 trait IsValid[A]
 object IsValid {
 
-  // sum $ multByPosition $ zipAccountNumberWithPosition numbers) `modNat` 5
-  // simplified: given 2 digits, it's valid if (A * 1 + b * 0) mod 5 == 0
   def apply[L <: HList](implicit ev: IsValid[L]) = ev
 
-  implicit def validNumber[L <: HList, H <: HList, A <: Nat, B <: Nat, P1 <: Nat, P2 <: Nat, S <: Nat](
-     implicit evA: IsHCons.Aux[L, A, H],
-     evB: IsHCons.Aux[H, B, HNil],
-     prod1: Prod.Aux[_1, A, P1],
-     prod2: Prod.Aux[_0, B, P2],
-     sum: Sum.Aux[P1, P2, S],
-     mod: Mod.Aux[S, _5, _0]
-   ): IsValid[L] = new IsValid[L] {}
+  //  account number:  3  4  5  8  8  2  8  6  5
+  //  position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
+  //
+  //  checksum calculation:
+  //    (d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
+  implicit def validNumber[D9 <: Nat, D8 <: Nat, D7 <: Nat, D6 <: Nat, D5 <: Nat, D4 <: Nat, D3 <: Nat, D2 <: Nat, D1 <: Nat,
+                           P9 <: Nat, P8 <: Nat, P7 <: Nat, P6 <: Nat, P5 <: Nat, P4 <: Nat, P3 <: Nat, P2 <: Nat, P1 <: Nat,
+                           S1 <: Nat, S2 <: Nat, S3 <: Nat, S4 <: Nat, S5 <: Nat, S6 <: Nat, S7 <: Nat, S8 <: Nat](
+    implicit p9: Prod.Aux[D9, _9, P9],
+    p8 : Prod.Aux[D8, _8, P8],
+    p7 : Prod.Aux[D7, _7, P7],
+    p6 : Prod.Aux[D6, _6, P6],
+    p5 : Prod.Aux[D5, _5, P5],
+    p4 : Prod.Aux[D4, _4, P4],
+    p3 : Prod.Aux[D3, _3, P3],
+    p2 : Prod.Aux[D2, _2, P2],
+    p1 : Prod.Aux[D1, _1, P1],
+    s1: Sum.Aux[P9, P8, S1],
+    s2: Sum.Aux[P7, S1, S2],
+    s3: Sum.Aux[P6, S2, S3],
+    s4: Sum.Aux[P5, S3, S4],
+    s5: Sum.Aux[P4, S4, S5],
+    s6: Sum.Aux[P3, S5, S6],
+    s7: Sum.Aux[P2, S6, S7],
+    s8: Sum.Aux[P1, S7, S8],
+    mod: Mod.Aux[S8, _11, _0]
+  ): IsValid[D9 :: D8 :: D7 :: D6 :: D5 :: D4 :: D3 :: D2 :: D1 :: HNil] = {
+    new IsValid[D9 :: D8 :: D7 :: D6 :: D5 :: D4 :: D3 :: D2 :: D1 :: HNil] {}
+  }
+
 
 }
---
+++ src/main/scala/net/d.scala
@@ -0,0 +1,84 @@
+package net
+
+import shapeless._
+import shapeless.nat._
+import shapeless.ops.nat.{Mod, Prod, Sum}
+
+trait SumHList[L, M] {
+  type S
+}
+object SumHList {
+
+  type Aux[L, M, O] = SumHList[L, M] {
+    type S = O
+  }
+
+  def apply[L <: HList, M <: HList](implicit ev: SumHList[L, M]): ev.type = ev
+
+  // credit to http://stackoverflow.com/questions/42115144/sum-of-two-identically-sized-hlists-at-compile-time/42115847#42115847
+  implicit def sumZippedInductiveEq5[LH <: Nat, L <: HList, MH <: Nat, M <: HList, RS <: Nat, CS <: Nat, E <: Nat](
+    implicit ev: SumHList.Aux[L, M, RS],
+    curr: Sum.Aux[LH, MH, CS],
+    total: Sum.Aux[CS, RS, E]
+  ): SumHList.Aux[LH :: L, MH :: M, E] = new SumHList[LH :: L, MH :: M] {
+    type S = E
+  }
+
+  implicit val hnils: SumHList.Aux[HNil, HNil, _0] = new SumHList[HNil, HNil] {
+    type S = _0
+  }
+}
+
+// Given an HList, provide evidence of the sum of HList
+// multiplied
+trait HListProductZipped[L] {
+  type Out
+}
+object HListProductZipped {
+
+  type Aux[L, O] = HListProductZipped[L] { type Out = O }
+
+  def apply[L <: HList](implicit ev: HListProductZipped[L]): ev.type = ev
+
+  implicit def sumTimesLength3[A <: Nat, B <: Nat, C <: Nat, X <: Nat, Y <: Nat, Z <: Nat, O <: HList](
+    implicit prodA: Prod.Aux[A, _3, X],
+             prodB: Prod.Aux[B, _2, Y],
+             prodC: Prod.Aux[C, _1, Z]
+  ) =
+    new HListProductZipped[A :: B :: C :: HNil] {
+      type Out = X :: Y :: Z :: HNil
+    }
+}
+
+trait HListSum[L] {
+  type Out
+}
+object HListSum {
+
+  type Aux[L, O] = HListSum[L] { type Out = O }
+
+  def apply[L <: HList](implicit ev: HListSum[L]): ev.type = ev
+
+  implicit def hListSumInductive[H <: Nat, L <: HList, S <: Nat, T <: Nat](
+    implicit rest: HListSum.Aux[L, T],
+             all: Sum.Aux[H, T, S]): HListSum.Aux[H :: L, S] = new HListSum[H :: L] {
+    type Out = S
+  }
+
+  implicit val hlistSumHNil: HListSum.Aux[HNil, _0] = new HListSum[HNil] {
+    type Out = _0
+  }
+}
+
+//trait IsValidInductive[L]
+//object IsValidInductive {
+//
+//  // valid criterion:
+//  // 3*d3 + 2*d2 + 1*d1 mod 11 == 0
+//  def apply[L <: HList](implicit ev: IsValidInductive[L]) = ev
+//
+//  implicit def wholeIsValid[LH <: Nat, L <: HList, MH <: Nat, M <: HList, P <: Nat](
+//   implicit ev: SumZippedProduct.Aux[H, _3, P]
+//            ): IsValidInductive[H :: L] = new IsValidInductive[H :: L] {}
+//
+//}
