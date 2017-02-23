bcb99b63b4b08f7082466379f651d7794a432f8d
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-02-22 22:49:42 -0500

    Add inductive 'HListProductZipped'.

--- build.sbt
+++ build.sbt
@@ -1,10 +1,6 @@
 scalaVersion := "2.12.1"
 
-scalacOptions := Seq("-Yinduction-heuristics")
-
-
 libraryDependencies ++= Seq(
   "com.chuusai" %% "shapeless" % "2.3.2"
 )
 
-scalaOrganization in ThisBuild := "org.typelevel"
--- src/main/scala/net/main.scala
+++ src/main/scala/net/main.scala
@@ -2,33 +2,8 @@ package net
 
 import shapeless._
 import shapeless.nat._
+import shapeless.ops.hlist.Length
 import shapeless.ops.nat.{Mod, Prod, Sum}
-
-// Given 2 HList's, consisting of Nat's entirely, add them together.
-trait SumHList[L, M] {
-  type S
-}
-object SumHList {
-
-  type Aux[L, M, O] = SumHList[L, M] {
-    type S = O
-  }
-
-  def apply[L <: HList, M <: HList](implicit ev: SumHList[L, M]): ev.type = ev
-
-  // credit to http://stackoverflow.com/questions/42115144/sum-of-two-identically-sized-hlists-at-compile-time/42115847#42115847
-  implicit def sumZippedInductiveEq5[LH <: Nat, L <: HList, MH <: Nat, M <: HList, RS <: Nat, CS <: Nat, E <: Nat](
-    implicit ev: SumHList.Aux[L, M, RS],
-    curr: Sum.Aux[LH, MH, CS],
-    total: Sum.Aux[CS, RS, E]
-  ): SumHList.Aux[LH :: L, MH :: M, E] = new SumHList[LH :: L, MH :: M] {
-    type S = E
-  }
-
-  implicit val hnils: SumHList.Aux[HNil, HNil, _0] = new SumHList[HNil, HNil] {
-    type S = _0
-  }
-}
 
 // Given an HList of size 3, provide evidence of the sum of HList
 // multiplied by _3 :: _2 :: _1 :: HNil
@@ -38,34 +13,21 @@ trait HListProductZipped[L <: HList] {
 }
 object HListProductZipped {
 
-  type Aux[L <: HList, O <: HList] = HListProductZipped[L] { type Out = O }
+  type Aux[L <: HList, Out1 <: HList] = HListProductZipped[L] { type Out = Out1 }
 
-  def apply[L <: HList](implicit ev: HListProductZipped[L]): ev.type = ev
+  def apply[L <: HList](implicit ev: HListProductZipped[L]): Aux[L, ev.Out] = ev
 
-  implicit def hlistProductZippedInductive[A <: Nat, B <: Nat, C <: Nat, D <: Nat, E <: Nat, F <: Nat, G <: Nat, H <: Nat, I <: Nat](
-    implicit prod9: Prod[A, _9],
-             prod8: Prod[B, _8],
-             prod7: Prod[C, _7],
-             prod6: Prod[D, _6],
-             prod5: Prod[E, _5],
-             prod4: Prod[F, _4],
-             prod3: Prod[G, _3],
-             prod2: Prod[H, _2],
-             prod1: Prod[I, _1]
-  ) =
-    new HListProductZipped[A :: B :: C :: D :: E :: F :: G :: H :: I :: HNil] {
-      type Out = prod9.Out :: prod8.Out :: prod7.Out :: prod6.Out :: prod5.Out :: prod4.Out :: prod3.Out :: prod2.Out :: prod1.Out :: HNil
-    }
+  implicit def induct[H <: Nat, T <: HList, L <: Nat](
+    implicit ev: Length.Aux[H :: T, L],
+             prod: Prod[H, L],
+             rest: HListProductZipped[T]
+  ): HListProductZipped.Aux[H :: T, prod.Out :: rest.Out] = new HListProductZipped[H :: T] {
+    type Out = prod.Out :: rest.Out
+  }
 
-//
-//  implicit def hlistProduceZippedInductive[A <: Nat, B <: Nat, C <: Nat, D <: Nat, E <: Nat, F <: Nat, G <: Nat, H <: Nat, I <: Nat](
-//    implicit prod9: Prod[A, _9],
-//             ev: HListProductZipped[B :: C :: D :: E :: F :: G :: H :: I :: HNil]
-//  ) =
-//    new HListProductZipped[A :: B :: C :: D :: E :: F :: G :: H :: I :: HNil] {
-//      type Out = prod9.Out :: ev.Out
-//    }
-
+  implicit val hnilHListProductZipped: HListProductZipped.Aux[HNil, HNil] = new HListProductZipped[HNil] {
+    type Out = HNil
+  }
 
 }
 
@@ -78,16 +40,10 @@ object HListSum {
 
   def apply[L <: HList](implicit ev: HListSum[L]): Aux[L, ev.Out] = ev
 
-//  implicit def hListSumInductive[H <: Nat, L <: HList, T <: Nat](
-//    implicit rest: HListSum.Aux[L, T],
-//             all: Sum[H, T]): HListSum.Aux[H :: L, all.Out] = new HListSum[H :: L] {
-//    type Out = all.Out
-//  }
-
-  implicit def hListSumInductive[H <: Nat, L <: HList, T <: Nat](
-    implicit rest: HListSum.Aux[L, T],
-    all: Sum[H, T]): HListSum.Aux[H :: L, all.Out] = new HListSum[H :: L] {
-    type Out = all.Out
+  implicit def hListSumInductive[H <: Nat, L <: HList, S <: Nat](
+    implicit rest: HListSum.Aux[L, S],
+             sum: Sum[H, S]): HListSum.Aux[H :: L, sum.Out] = new HListSum[H :: L] {
+    type Out = sum.Out
   }
 
   implicit val hlistSumHNil: HListSum.Aux[HNil, _0] = new HListSum[HNil] {
@@ -96,21 +52,20 @@ object HListSum {
 }
 
 
+//  account number:  3  4  5  8  8  2  8  6  5
+//  position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
+//
+//  checksum calculation:
+//    (d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
 trait IsValidInductive[L <: HList]
 object IsValidInductive {
-
-  //  account number:  3  4  5  8  8  2  8  6  5
-  //  position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
-  //
-  //  checksum calculation:
-  //    (d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
   def apply[L <: HList](implicit ev: IsValidInductive[L]) = ev
 
   implicit def wholeIsValid[H <: Nat, T <: HList, L <: HList, S <: Nat](
    implicit ev: HListProductZipped.Aux[H :: T, L],
             sum: HListSum.Aux[L, S],
-            mod: Mod.Aux[S, _11, _0]
+            mod: Mod.Aux[S, _11, _0],
+            length: Length.Aux[H :: T, _9]
             ): IsValidInductive[H :: T] = new IsValidInductive[H :: T] {}
-
 
 }
